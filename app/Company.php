4c31c2ae0e9161860c048a029cf99f6006cdad07
Manuel Entrena <manuel.entrena@gmail.com>
2021-10-21 10:28:43 +0200

    Added drop timestamps all tables

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    public $timestamps = false;
 }
---
+++ database/migrations/2021_10_21_081539_create_delete_all_times_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeleteAllTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+        Schema::table('cities', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('cities', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('companies', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('projects', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+}
